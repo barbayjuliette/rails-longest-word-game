733a73366703d4950981812410a283691236cd3c
juliette barbay <barbayjuliette95@gmail.com>
2022-11-08 18:04:24 +0800

    checked validity of word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,42 @@
+class GamesController < ApplicationController
+  def new
+    @grid = []
+    @array_of_letters = ('A'..'Z').to_a
+    10.times { @grid << @array_of_letters.sample }
+  end
+
+  def score
+    grid = params[:grid].split(" ")
+    @word = params[:word]
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    api = JSON.parse(URI.open(url).read)
+
+    @result =
+      if only_grid_letters(grid) == false
+        "Sorry but #{@word.upcase} cannot be built out of #{grid.join(" ")}"
+      elsif api[:found] == false
+        "Sorry but #{@word.upcase} is not a valid english word"
+      else
+        "Congratulations! #{@word.upcase} is a valid word"
+      end
+  end
+
+  private
+
+  def only_grid_letters(grid)
+    array_of_letters = @word.upcase.chars
+    array_of_letters.each do |letter|
+      return false unless grid.include?(letter)
+
+      index = grid.find_index(letter)
+      grid.delete_at(index)
+    end
+  end
+
+  def check_valid_word
+
+
+  end
+
+
+end
